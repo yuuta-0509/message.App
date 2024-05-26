2a6e5ff0a28c4097ef68bb092b578f5d57b57f00
yuuta-0509 <140583267+yuuta-0509@users.noreply.github.com>
2024-05-27 01:22:52 +0900

    一区切り

--- app.js
+++ app.js
@@ -3,67 +3,46 @@ var cityId = 1850147;
 var cityPush = document.getElementById('cityName');
 var tempPush = document.getElementById('tempValue');
 var weatherPush = document.getElementById('weatherValue');
-async function getWeather () {
-    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=${apiKey}`
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const weatherResult = await data.weather[0].main;
-        let translatedWeather = '';
-        switch (weatherResult) {
-        case 'Thunderstorm':
-            translatedWeather = '雷雨';
-            break;
-        case 'Drizzle':
-            translatedWeather = '霧雨';
-            break;
-        case 'Rain':
-            translatedWeather = '雨';
-            break;
-        case 'Snow':
-            translatedWeather = '雪';
-            break;
-        case 'Mist':
-            translatedWeather = '霧';
-            break;
-        case 'Smoke':
-            translatedWeather = '煙';
-            break;
-        case 'Haze':
-            translatedWeather = '霞';
-            break;
-        case 'Dust':
-            translatedWeather = 'ほこり';
-            break;
-        case 'Fog':
-            translatedWeather = '霧';
-            break;
-        case 'Sand':
-            translatedWeather = '砂';
-            break;
-        case 'Ash':
-            translatedWeather = '灰';
-            break;
-        case 'Squall':
-            translatedWeather = '突風';
-            break;
-        case 'Tornado':
-            translatedWeather = '竜巻';
-            break;
-        case 'Clear':
-            translatedWeather = '晴れ';
-            break;
-        case 'Clouds':
-            translatedWeather = '曇り';
-            break;
-        default:
-            translatedWeather = weatherResult; 
+var selectElement = document.getElementById("map-select");
+var output = document.getElementById('output');
+selectElement.addEventListener("change", function() {
+    cityId = selectElement.value || cityId;
+    async function getWeather () {
+        var apiUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&lang=ja&appid=${apiKey}`;
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            const weatherResult = await data.weather[0].main;
+            const weatherTranslations = {
+                Thunderstorm: '雷雨',
+                Drizzle: '霧雨',
+                Rain: '雨',
+                Snow: '雪',
+                Mist: '霧',
+                Smoke: '煙',
+                Haze: '霞',
+                Dust: 'ほこり',
+                Fog: '霧',
+                Sand: '砂',
+                Ash: '灰',
+                Squall: '突風',
+                Tornado: '竜巻',
+                Clear: '晴れ',
+                Clouds: '曇り'
+            };
+            const translatedWeather = weatherTranslations[weatherResult] || weatherResult;
+            const areaResult = await data.name;
+            const tempResult = await data.main.temp;
+            console.log(data);
+            output.innerHTML = 
+            '<p>現在の: ' + areaResult + 'の気温は' + tempResult + '℃</p>' +
+            '<p>天気は: ' + translatedWeather + ' °Cです</p>';
+        } catch (error) {
+            console.error('Error fetching the weather data:', error);
         }
-        weatherPush.innerText = translatedWeather;
-        console.log(pushData);
-        console.log(data)
-    } catch (error) {
-        console.error('Error fetching the weather data:', error);
     }
-}
-getWeather();
+    getWeather();
+});
+
+        // var apiUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&appid=${apiKey}`;
+
